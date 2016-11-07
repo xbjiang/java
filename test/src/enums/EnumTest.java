f2b351fc100efafb066387cca56e5a92a4398137
xbjiang <jxavierphysics@163.com>
2016-11-07 21:38:33 +0800

    change file

--- test/src/enums/EnumTest.java
+++ test/src/enums/EnumTest.java
@@ -3,25 +3,25 @@ package enums;
 import java.util.*;
 
 public class EnumTest {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter a size: (SMALL, MEDIUM, LARGE, EXTRA_LARGE) ");
+		System.out.println("Please enter a size:{SMALL, MEDIUM, LARGE, EXTRA_LARGE}");
 		String input = in.next().toUpperCase();
+		
 		Size size = Enum.valueOf(Size.class, input);
-		System.out.println("size=" + size);
-		System.out.println("abbreviation=" + size.getAbbreviation());
+		System.out.println("size:"+size);
+		System.out.println("abbreviation:"+size.getAbbreviation());
 		if (size == Size.EXTRA_LARGE)
-			System.out.println("Good job--you paid attention to the _.");
+			System.out.println("Good job, you paid attention to the _.");
 	}
-	
 }
 
 enum Size {
 	SMALL("S"), MEDIUM("M"), LARGE("L"), EXTRA_LARGE("XL");
 	
-	private Size(String abbreviation) { this.abbreviation = abbreviation;}
-	public String getAbbreviation() { return abbreviation; }
-	
 	private String abbreviation;
+	
+	private Size(String abbreviation) {this.abbreviation = abbreviation;};
+	public String getAbbreviation() {return abbreviation;};
 }
-
