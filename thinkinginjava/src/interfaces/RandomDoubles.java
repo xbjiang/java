1919a3452c0599cefa11de0e6d09547f05e9181c
xbjiang <jxavierphysics@163.com>
2016-11-18 11:45:53 +0800

    daily practices

---
+++ thinkinginjava/src/interfaces/RandomDoubles.java
@@ -0,0 +1,12 @@
+package interfaces;
+import java.util.*;
+
+public class RandomDoubles {
+    public static Random rand = new Random(47);
+    public double next() {return rand.nextDouble();}
+    public static void main(String[] args) {
+    	RandomDoubles rd = new RandomDoubles();
+    	for (int i = 0; i < 7; i++)
+    		System.out.print(rd.next() + " ");
+    }
+}
