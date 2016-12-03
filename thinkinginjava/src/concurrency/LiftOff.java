7aeb037281a82e194a7cc869ee30a36b80e8bc53
xbjiang <jxavierphysics@163.com>
2016-12-03 19:03:05 +0800

    concurrency practices

---
+++ thinkinginjava/src/concurrency/LiftOff.java
@@ -0,0 +1,22 @@
+package concurrency;
+
+public class LiftOff implements Runnable {
+	protected int countDown = 10;
+	private static int taskCount = 0;
+	private final int id = taskCount++;
+	public LiftOff() {}
+	public LiftOff(int count) {countDown = count;}
+	public String status()
+	{
+		return "#" + id + "(" + 
+				(countDown > 0 ? countDown : "LiftOff!") + "), ";
+	}
+	public void run()
+	{
+		while (countDown-- > 0)
+		{
+			System.out.print(status());
+			Thread.yield();
+		}
+	}
+}
